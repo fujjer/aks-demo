209d5484b78decc14dc6b69fdb7d0189c962abfb
fujjer <147313406+fujjer@users.noreply.github.com>
2024-02-20 18:49:10 +0400

    Update remote.sh

--- remote.sh
+++ remote.sh
@@ -5,7 +5,7 @@ STORAGE_ACCOUNT_NAME=fujjer$RANDOM
 CONTAINER_NAME=containertfstate
 
 # Create resource group
-az group create --name $RESOURCE_GROUP_NAME --location uaenorth
+az group create --name $RESOURCE_GROUP_NAME --location westeurope
 
 # Create storage account
 az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
